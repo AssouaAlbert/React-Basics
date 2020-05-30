172c4a8bd86c642e90d8bb56838caed57b408913
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-05-31 02:06:42 +0300

    First React Application

---
+++ precache-manifest.48cc8645b22fafa87de41edb05dcaa93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82ea82f5cdb89dc0487a46cadce559",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "e282c832efc62d4e4fa8",
+    "url": "/Monster-Rolodex/static/css/main.d43e8ffb.chunk.css"
+  },
+  {
+    "revision": "d687c1f9d751eecf4fb8",
+    "url": "/Monster-Rolodex/static/js/2.1dd7b907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.1dd7b907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e282c832efc62d4e4fa8",
+    "url": "/Monster-Rolodex/static/js/main.add7e298.chunk.js"
+  },
+  {
+    "revision": "39754c773e1d7fb81761",
+    "url": "/Monster-Rolodex/static/js/runtime-main.b0e90f1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
